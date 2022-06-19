2b5359356f6f6a9c4406f685d4f6b0ff897a5e45
typan <sampty@student.ubc.ca>
2022-06-18 18:38:39 -0700

    set up dependencies, add data link ignored, add config file, add train and test scripts

---
+++ scripts/test_nerf_synthetic.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# define shape names
+declare -a scene_names=(
+    "chair"
+    "drums"
+    "ficus"
+    "hotdog"
+    "lego"
+    "materials"
+    "mic"
+    "ship"
+    )
+
+for scene_name in ${scene_names[@]}; do
+    python run_nerf.py \
+    --config configs/${scene_name}.txt \
+    --render_only \
+    --render_test | tee logs/blender_paper_${scene_name}/test.log
+done
