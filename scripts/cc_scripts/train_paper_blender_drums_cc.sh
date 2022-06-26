7ef516cab0e99bac6b6a0d29fcb08908537779f8
eric <cgx1997@outlook.com>
2022-06-26 12:18:27 -0700

    train lego+other scenes for 96 hours each on CC

--- scripts/cc_scripts/train_paper_blender_drums_cc.sh
+++ scripts/cc_scripts/train_paper_blender_drums_cc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --time=96:00:00
 #SBATCH --account=def-rhodin
-#SBATCH --job-name=tr_lego_nerf-tf
+#SBATCH --job-name=tr_drums_nerf-tf
 #SBATCH --gres=gpu:v100l:1
 #SBATCH --mem=24G
 module load StdEnv/2016.4
@@ -12,4 +12,4 @@ module load imagemagick
 cd /home/gxc321/
 source NerfEnv/bin/activate
 cd /home/gxc321/scratch/nerf/
-source scripts/train.sh paper blender lego 20000 1000000
+source scripts/train.sh paper blender drums 20000 1000000
