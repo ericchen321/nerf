f8d41e3eba4ebda1c835ccc8afe4e314b9dd3c93
eric <cgx1997@outlook.com>
2022-07-01 18:25:23 -0700

    reorganized file struct; polished local scripts for LLFF data

--- scripts/purkinje_scripts/train_paper_llff.sh
+++ scripts/purkinje_scripts/train_paper_llff.sh
@@ -6,26 +6,21 @@ chunk="$3"
 netchunk="$4"
 
 exp_category="paper"
-dataset_name="blender"
+dataset_name="llff"
 
 # define scene names
 declare -a scene_names=(
     "fern"
-    # "flower"
-    # "fortress"
-    # "horns"
-    # "leaves"
-    # "orchids"
-    # "room"
-    # "trex"
+    "flower"
+    "fortress"
+    "horns"
+    "leaves"
+    "orchids"
+    "room"
+    "trex"
     )
 
 for scene_name in ${scene_names[@]}; do
-    source scripts/train.sh paper llff ${scene_name} 50 101 32768 65536
-    # python run_nerf.py \
-    # --config configs/${exp_category}_configs/${dataset_name}_configs/${scene_name}.txt \
-    # --i_img=$i_img \
-    # --N_iters $N_iters \
-    # --chunk=$chunk \
-    # --netchunk=$netchunk
+    source scripts/train.sh \
+    $exp_category $dataset_name $scene_name $i_img $N_iters $chunk $netchunk
 done
--- scripts/train.sh
+++ scripts/train.sh
@@ -8,11 +8,23 @@ N_iters="$5"
 chunk="$6"
 netchunk="$7"
 
+# get dataset dir based on name
+dataset_dir=""
+if [[ "$dataset_name" == "blender" ]]; then
+    dataset_dir="data/nerf_synthetic/"
+elif [[ "$dataset_name" == "llff" ]]; then
+    dataset_dir="data/nerf_llff_data/"
+else
+    echo "Error! Dataset name not recognized"
+    exit 1
+fi
+
+set +x
 python run_nerf.py \
-    --config configs/${exp_category}_configs/${dataset_name}_configs/${dataset_name}_config.txt \
+    --config configs/${dataset_name}_configs/${exp_category}.txt \
     --i_img=$i_img \
     --N_iters $N_iters \
     --chunk=$chunk \
     --netchunk=$netchunk \
-    --expname=llff_paper_$scene_name \
-    --datadir=./data/nerf_llff_data/$scene_name
+    --expname=${dataset_name}_${exp_category}_${scene_name} \
+    --datadir=$dataset_dir/$scene_name
